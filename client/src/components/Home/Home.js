dc69df89562d95f10aba79379e6cd33ce29188eb
whizkevina <wizkevinaaina@gmail.com>
2020-02-16 21:56:30 -0800

    added navbar and logo

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,48 +1,29 @@
 import React, { Component } from "react";
 import "./HomeStyle.css";
 import { Link } from "react-router-dom";
+import Navbar from "../layouts/Nav/navbar";
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <div id="page">
-          <nav className="gtco-nav" role="navigation">
-            <div className="gtco-container">
-              <div className="row">
-                <div className="col-xs-2">
-                  <div id="gtco-logo"><a href="/">FT CHOIR</a></div>
-                </div>
-                <div className="col-xs-8 text-center menu-1">
-                  <ul>
-                    <li><a href="/contact">Contact</a></li>
-                  </ul>
-                </div>
-                <div className="col-xs-2 text-right hidden-xs menu-2">
-                  <ul>
-                    <li className="btn-cta"><a href="/login"><span>Login</span></a></li>
-                    <li className="btn-cta"><a href="/Signup"><span>Signup</span></a></li>
-                  </ul>
-                </div>
-              </div>
-
+        <Navbar/>
+        <div className="container-fluid home-main">
+          <h2 className="intro-backdrop">
+            <p>Faith Tabernacle Choir Directory <span className="blinker">.</span></p>
+          </h2>
+          <div className="row">
+            <div className="col-md-6">
+              <Link className="btn btn-outline-success btn-lg d-block" to="/signup">
+                Signup
+              </Link>
             </div>
-          </nav>
-
-          <header id="gtco-header" className="gtco-cover home-main" role="banner">
-            <div className="gtco-container">
-              <div className="row">
-                <div className="col-md-8 col-md-offset-2 text-center">
-                  <div className="display-t">
-                    <div className="display-tc animate-box" data-animate-effect="fadeIn">
-                      <h1>FT Choir Directory</h1>
-                      <p><a href="/signup" className="btn btn-default">Get Started</a></p>
-                    </div>
-                  </div>
-                </div>
-              </div>
+            <div className="col-md-6">
+              <Link className="btn btn-outline-primary btn-lg d-block" to="/login">
+                Login
+              </Link>
             </div>
-          </header>
+          </div>
         </div>
         <div className="container-fluid home-content1">
           <div className="row">
@@ -62,18 +43,7 @@ class Home extends Component {
                 mollit fugiat nostrud nisi occaecat nisi ea adipisicing. Laborum
                 sunt ipsum non elit cupidatat esse. Elit ipsum labore sit labore
                 dolore tempor esse. Ad incididunt aute sunt consequat amet.
-                Consectetur esse aute qui duis reprehenderit elit labore mollit
-                deserunt consectetur enim laboris. Incididunt ad pariatur magna
-                id. Sint ut culpa qui Lorem sunt sint qui fugiat elit qui sunt
-                commodo. Nisi ex exercitation eu do velit consectetur Lorem sunt
-                culpa ullamco irure ut magna ipsum. Ex est mollit excepteur
-                velit est esse veniam nostrud voluptate reprehenderit officia
-                fugiat. Ex irure magna ipsum sunt magna magna cillum irure aute
-                ut laborum. Esse Lorem qui deserunt consectetur deserunt quis
-                pariatur reprehenderit commodo incididunt. Laborum eiusmod
-                excepteur irure cupidatat reprehenderit anim reprehenderit. Ex
-                ipsum ad sint nostrud cillum commodo consequat sit cillum
-                voluptate.
+
               </p>
             </div>
           </div>
